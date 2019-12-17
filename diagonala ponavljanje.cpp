24b6ddf4416e386541a2a9a7907283ed3f7861e3
gluhak <58471951+gluhak@users.noreply.github.com>
2019-12-17 14:03:57 +0100

    Add files via upload

---
+++ diagonala ponavljanje.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int upis,x,i;
+	
+	
+	cout<<"Upisi broj veci od 2 i manji od 100"<<endl;
+	cin>>upis;
+	
+           
+
+            int polje[upis][upis]={ {1,2,3},
+									{4,5,6}};
+									
+			for(int i=2;i<=100;i++){
+			
+			cout<<polje;
+			}
+}
+
+
+
+            
+            return 0;
+			system("PAUSE"); 
+}
